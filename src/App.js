01d6414b0c037555f026684af4f19dc0cfaa75fe
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2022-01-30 01:19:14 +1000

    add home and not found pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import React, {Fragment} from 'react';
+import React from 'react';
 import Navbar from "./components/layout/navabr";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import About from "./components/pages/about";
 import User from "./components/users/User";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 const App = () => {
     return (
@@ -20,17 +20,10 @@ const App = () => {
                     <div className={"container"}>
                         <Alert/>
                         <Routes>
-                            <Route
-                                exact
-                                path={"/"}
-                                element={
-                                <Fragment>
-                                    <Search/>
-                                    <Users/>
-                                </Fragment>
-                            }/>
+                            <Route exact path={"/"} element={<Home/>}/>
                             <Route exact path={"/about"} element={<About/>} />
                             <Route exact path={'/user/:login'} element={<User/>}/>
+                            <Route path={"*"} element={<NotFound/>} />
                         </Routes>
                     </div>
                 </div>
